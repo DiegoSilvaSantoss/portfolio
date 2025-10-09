49ae601a3c9da31a47594863fe5b1f75810952ad
Dhii Santos <diegoalphapro@gmail.com>
2025-10-09 06:37:26 -0300

    reajustes

--- javascript.js
+++ javascript.js
@@ -1,11 +1,11 @@
 alert(`
-    <> Informações do Desenvolvedor
+    < > INFORMAÇÕES DO DESENVOLVEDOR
 
     📢ATENÇÃO❗
 
     👨‍🔧: Site em Manutenção.
 
-    🗃️: Vou colocar os projetos real, esse que estão no Portfólio são fotos ilustrativos... Vou pôr os projetos real e com modo de visualização. assim tendo uma melhor experiência.
+    🗃️: Vou colocar os projetos de fato, esses que estão no Portfólio são fotos ilustrativos... Vou pôr os projetos real e com modo de visualização. assim tendo uma melhor experiência.
     
     ⚙️: Ainda vou finalizar na parte JAVASCRIPT, no formulário de 📱 CONTATO no site (Portfólio).
     
