1d2097f26945300285342c75f84e984730cf60ad
Dhii Santos <diegoalphapro@gmail.com>
2025-10-08 22:35:29 -0300

    reajustes

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-alert("Site em manutenção. Estou incluindo os projetos :)")
+alert(`
+    <> Informações do Desenvolvedor
+
+    📢ATENÇÃO❗
+
+    👨‍🔧: Site em Manutenção.
+
+    🗃️: Vou colocar os projetos real, esse que estão no Portfólio são fotos ilustrativos... Vou pôr os projetos real e com modo de visualização. assim tendo uma melhor experiência.
+    
+    ⚙️: Ainda vou finalizar na parte JAVASCRIPT, no formulário de 📱 CONTATO no site (Portfólio).
+    
+    `) 
