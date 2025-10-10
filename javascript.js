f8282005c90002deda0ccf6d49d61cdbe13901b2
Dhii Santos <diegoalphapro@gmail.com>
2025-10-10 00:43:10 -0300

    reajustes

--- javascript.js
+++ javascript.js
@@ -6,6 +6,10 @@ alert(`
     👨‍🔧: Site em Manutenção.
 
     🗃️: Vou colocar os projetos de fato, esses que estão no Portfólio são fotos ilustrativos... Vou pôr os projetos real e com modo de visualização. assim tendo uma melhor experiência.
+
+    ✔️: Projeto (Página de Viagens), Já funcionando.
+    ❌: Projeto (Lojas de Carros), Tá em andamento...
+    ❌: Projeto (Microsoft), Tá em andamento...
     
     ⚙️: Ainda vou finalizar na parte JAVASCRIPT, no formulário de 📱 CONTATO no site (Portfólio).
     
